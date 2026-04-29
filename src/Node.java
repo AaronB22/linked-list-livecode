1c3f8ac54990023a7fc93129a0251a302b73daa5
AaronB22 <aaronthesnowboarder@live.com>
2026-04-29 00:13:48 -0700

    livecode

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,9 @@
 public class Node {
-    
+    public char value;
+    public Node next;
+
+    public Node(char c, Node next){
+        this.value=c;
+        this.next=next;
+    }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('w',null);
+
+        Node otherNode= new Node('x',null);
+  
+        myNode.next=otherNode;
+
+        Node coolNode= new Node('e',null);
+        otherNode.next=coolNode;
+
+
+
+        // System.out.println(myNode.value);
+        // System.out.println(myNode.next.value);
+        // System.out.println(coolNode.value);
+        Node current= myNode;
+        while(current!=null){
+            System.out.println(current.value);
+            current= current.next;
+        }
     }
 }
