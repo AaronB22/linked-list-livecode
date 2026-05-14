175075472d307af7deb4380922246476244263c2
AaronB22 <aaronthesnowboarder@live.com>
2026-05-14 11:15:22 -0700

    delete method

--- src/Practice.java
+++ src/Practice.java
@@ -7,17 +7,43 @@ public class Practice {
         myNode.next=otherNode;
 
         Node coolNode= new Node('e',null);
+        Node helloNode= new Node('f',null);
         otherNode.next=coolNode;
-
-
+        coolNode.next=helloNode;
+        // int numNodes= length(myNode);
+        // System.out.println(numNodes);
+        Node newHead= removeAt(myNode, 2);
 
         // System.out.println(myNode.value);
         // System.out.println(myNode.next.value);
         // System.out.println(coolNode.value);
-        Node current= myNode;
+        Node current= newHead;
         while(current!=null){
             System.out.println(current.value);
             current= current.next;
         }
+    }
+
+    public static int length(Node head){
+        int count=0;
+        Node current=head;
+        while(current!=null){
+            count++;
+            current=current.next;
+        }
+        return count;
+    }
+
+    public static Node removeAt(Node head, int removeIdx){
+        if(removeIdx==0){
+            return head.next;
+        }
+        Node current= head;
+        
+        for(int i=0; i<removeIdx-1;i++){
+            current=current.next;
+        }
+        current.next=current.next.next;
+        return head;
     }
 }
